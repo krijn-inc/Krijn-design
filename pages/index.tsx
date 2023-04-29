42a24dfece91056abb5a6ecf72b2785e7051365e
Sjors van Holst <sjorsvanholst@gmail.com>
2023-04-29 10:09:40 +0200

    📱 Refactored CSS to SASS

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import { Inter } from 'next/font/google';
-import styles from '@/styles/Home.module.css';
+import styles from '@/styles/Home.module.scss';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,7 +13,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.container} ${inter.className}`}>
+      <main>
         <section className={`${styles.intro}`}>
           <h1 className={`${styles.heading}`}>
             <span>Design websites faster.</span>
